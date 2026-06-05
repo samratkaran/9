1cdfdb164cfe3f6236ab81f88bd7369f16cb7fd3
Karan Samrat <Karan@MacBook-Air.local>
2026-06-05 15:07:37 +0530

    learn basic things

---
+++ 03bgChanger/src/App.jsx
@@ -0,0 +1,24 @@
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ const [color, setColor] = useState('olive')
+let arr = [ 'white', "green" , "orange", "yellow", "blue", "pink", "black"]
+
+
+
+  return (
+    <div className='w-full h-screen' style={{backgroundColor:color}}>
+    <div className='mt-5  bg-white border-2 rounded-2xl border-amber-950 flex justify-center align-middle gap-8'>
+    {arr.map((val)=><button key={val} style={{backgroundColor:val , color:"black", cursor:"pointer"}}  className='border-2 p-1 rounded-2xl m-1 font-bold' onClick={()=>setColor(val)} > {val}</button>)}
+
+    </div>
+    <div>
+      
+    </div>
+    </div>
+  )
+}
+
+export default App
