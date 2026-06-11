de189cfbb486d3f03a27b1c74007b9fada3b55bf
Karan Samrat <Karan@MacBook-Air.local>
2026-06-11 16:35:00 +0530

    completed

--- 08ContextProjectTwo/src/App.jsx
+++ 08ContextProjectTwo/src/App.jsx
@@ -1,12 +1,40 @@
+import { useState } from "react"
+import { ThemeProvider } from "./Context/MainContext"
+import { useEffect } from "react"
+import ThemeBtn from "./Components/ThemeBtn"
+import Card from "./Components/Card"
 
 
 function App() {
-  
+  const [themeMode ,setThemeMode] = useState('light')
+
+  const darkTheme = ()=>{
+    setThemeMode('dark')
+  }
+
+    const lightTheme = ()=>{
+    setThemeMode('ligth')
+  }
+
+useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
 
   return (
-   <>
-    <h2 className="text-red-700">hello</h2>
-   </>
+    <ThemeProvider value={{themeMode ,darkTheme ,lightTheme}}>
+ <div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        <ThemeBtn/>
+                    </div>
+<Card/>
+                    <div className="w-full max-w-sm mx-auto">
+                       
+                    </div>
+                </div>
+            </div>
+            </ThemeProvider>
   )
 }
 
