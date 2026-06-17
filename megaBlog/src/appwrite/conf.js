8ca4407e16d46da78f9646f8cdcb8b858630de12
Karan Samrat <karansamrat05039@gmail.com>
2026-06-17 12:39:55 +0530

    more functionality added

--- megaBlog/src/appwrite/conf.js
+++ megaBlog/src/appwrite/conf.js
@@ -1,5 +1,5 @@
 import Config from "../config/Config";
-import {Client, Account , ID , TablesDB, Storage, Query} from 'appwrite'
+import {Client , TablesDB, Storage, Query, ID} from 'appwrite'
 
 export class Service{
 
@@ -34,9 +34,120 @@ export class Service{
 
     }
 
-    async updatePost(){
+    async updatePost(slug,{title, content, featuredImage, status}){
+         try {
+          return await this.tablesDB.updateRow({
+                databaseId:Config.appwriteDatabaseId,
+                tableId:Config.appwriteTableId,
+                rowId:slug,
+                data:{
+                title, content, featuredImage, status
+                }
+            })
+        } catch (error) {
+            console.log('updatepost error' , error)
+        }
 
     }
+
+    async deletePost(slug){
+ try {
+           await this.tablesDB.deleteRow({
+                databaseId:Config.appwriteDatabaseId,
+                tableId:Config.appwriteTableId,
+                rowId:slug,
+                
+            })
+            return true
+        } catch (error) {
+            console.log('deletepost error' , error)
+            return false
+        }
+    }
+
+       async getPost(slug){
+ try {
+          return await this.tablesDB.getRow({
+                databaseId:Config.appwriteDatabaseId,
+                tableId:Config.appwriteTableId,
+                rowId:slug,
+                
+            })
+            
+        } catch (error) {
+            console.log('get post error error' , error)
+            return false
+        }
+    }
+
+        async getPostList(queries = Query.equal("status" ,"active")){
+ try {
+          return await this.tablesDB.listRows({
+                databaseId:Config.appwriteDatabaseId,
+                tableId:Config.appwriteTableId,
+               queries:queries,
+               
+                
+            })
+            
+        } catch (error) {
+            console.log('get post error error' , error)
+            return false
+        }
+    }
+
+
+    // file Upload Mehtods
+
+    async uploadFile(file){
+        try {
+          return await this.storage.createFile({
+                bucketId:this.storage.appwriteStorageId,
+                fileId: ID.unique(),
+                file
+               
+                
+            })
+            
+        } catch (error) {
+            console.log('Upload File error error' , error)
+            return false
+        }
+    }
+
+        async deleteFile(fileId){
+        try {
+          return await this.storage.deleteFile({
+                bucketId:this.storage.appwriteStorageId,
+                fileId:fileId
+                
+               
+                
+            })
+            
+        } catch (error) {
+            console.log('Delete File error error' , error)
+            return false
+        }
+    }
+     
+    async getFilePreview(fileId){
+
+           try {
+          return await this.storage.getFilePreview({
+                bucketId:this.storage.appwriteStorageId,
+                fileId:fileId
+                
+               
+                
+            })
+            
+        } catch (error) {
+            console.log('Delete File error error' , error)
+            return false
+        }
+    }
+
 
 }
 
