5aee96e7c22dca324596f259506c08ea15a56e34
Karan Samrat <karansamrat05039@gmail.com>
2026-06-18 12:12:00 +0530

    few components added

---
+++ megaBlog/src/components/Input.jsx
@@ -0,0 +1,24 @@
+import { useId } from "react";
+
+function input({ lable, type = "text", className = "", ref, ...props }) {
+  const id = useId;
+  return(
+   <div className="w-full">
+   {lable && <lable className='inline-block mb-1 pl-1' htmlFor={id}>
+{lable}
+   </lable>}
+   <input 
+    id={id}
+    ref={ref}
+    type={type}
+    {...props}
+    className={`${className} px-3 py-2 rounded-lg bg-white text-black 
+          outline-none focus:bg-gray-50 duration-200 
+          border border-gray-200 w-full`}
+
+   />
+
+   </div>)
+}
+
+export default input;
